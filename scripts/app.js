3e8608dec0a5e23fd6ca930d832cc9f2ad186602
Muyiwa Olabinjo <89519352+olaa234@users.noreply.github.com>
2022-10-21 15:49:50 +0200

    adding icons and image to weather app

---
+++ scripts/app.js
@@ -0,0 +1,94 @@
+// Dom manipulation.
+
+const key = "T67dyeXctsP7f4kpCwqqJSl4vv3iqzyh";
+
+// get weather information
+const getWeather = async (id) => {
+  const base = "http://dataservice.accuweather.com/currentconditions/v1/";
+  const query = `${id}?apikey=${key}`;
+  const response = await fetch(base + query);
+  const data = await response.json();
+  return data[0];
+};
+
+//  get city info
+const getCity = async (city) => {
+  const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
+  const query = `?apikey=${key}&q=${city}`;
+  const response = await fetch(base + query);
+  const data = await response.json();
+  return data[0];
+};
+
+getCity("lagos")
+  .then((data) => {
+    return getWeather(data.Key);
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => console.log(err));
+
+///////////////////////////////////////////////////////////////////
+
+const cityForm = document.querySelector("form");
+const card = document.querySelector(".card");
+const details = document.querySelector(".details");
+const time = document.querySelector("img.time");
+const icon = document.querySelector(".icon img");
+
+const updateUI = (data) => {
+  //     console.log(data);
+  //   const cityDets = data.cityDets;
+  //   const weather = data.weather;
+
+  // destructure properties
+
+  const { cityDets, weather } = data;
+
+  // update details template
+  details.innerHTML = `
+         <h5 class="my-3">${cityDets.EnglishName}</h5>
+          <div class="my-3">${weather.WeatherText}</div>
+          <div class="display-4 my-4"><span>${weather.Temperature.Metric.Value}</span></div>
+        <span>&deg;C</div>
+        </div>
+  `;
+  /// update night and day Icon images
+
+  const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+  icon.setAttribute("src", iconSrc);
+
+  let timeSrc = null;
+  if (weather.IsDayTime) {
+    timeSrc = "img/day.svg";
+  } else {
+    timeSrc = "img/night.svg";
+  }
+
+  time.setAttribute("src", timeSrc);
+
+  // remove the d-none class if present
+  if (card.classList.contains("d-none")) {
+    card.classList.remove("d-none");
+  }
+};
+
+const updateCity = async (city) => {
+  const cityDets = await getCity(city);
+  const weather = await getWeather(cityDets.Key);
+  return { cityDets, weather };
+};
+
+cityForm.addEventListener("submit", (e) => {
+  // prevent Default action
+  e.preventDefault();
+
+  // get city value
+  const city = cityForm.city.value.trim();
+  cityForm.reset();
+  // update the UI with the new city
+  updateCity(city)
+    .then((data) => updateUI(data))
+    .catch((err) => console.log(err));
+});
